146db9df5111b20b0fdf034a6055d2871556af6b
Dia1208 <dia.gonzalezb@duocuc.cl>
2026-06-04 13:09:57 -0400

    Implementar el método findByUsuarioId en PublicacionRepositor
    Extend PublicacionRepository to include method for finding publications by user ID.

--- ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Repository/PublicacionRepository.java
+++ ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Repository/PublicacionRepository.java
@@ -1,4 +1,12 @@
 package com.socialcloud.ms_publicaciones.Repository;
 
-public interface PublicacionRepository {
+import com.socialcloud.ms_publicaciones.Entity.Publicacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
+
+    List<Publicacion> findByUsuarioId(Long usuarioId);
+
 }
