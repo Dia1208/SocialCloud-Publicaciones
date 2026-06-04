2cd6c6f873920b6b8196cdbc115bce2a20e5156f
Dia1208 <dia.gonzalezb@duocuc.cl>
2026-06-04 13:13:24 -0400

    Agregar JwtService para la gestión de tokens JWT
    Implemented JWT token validation and signing key retrieval.

--- ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Security/JwtService.java
+++ ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Security/JwtService.java
@@ -1,4 +1,29 @@
 package com.socialcloud.ms_publicaciones.Security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY =
+            "socialcloud_secret_key_2026_super_segura_para_jwt_123456";
+
+    private Key getSigningKey() {
+
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public Claims validarToken(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
