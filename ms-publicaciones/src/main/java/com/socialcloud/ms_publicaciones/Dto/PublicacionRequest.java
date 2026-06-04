e7d7825a1aa98418c851648f2ae963e069071a03
Dia1208 <dia.gonzalezb@duocuc.cl>
2026-06-04 13:06:51 -0400

    Implement PublicacionRequest with validation
    Added validation annotations for usuarioId and contenido fields.

--- ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Dto/PublicacionRequest.java
+++ ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Dto/PublicacionRequest.java
@@ -1,4 +1,17 @@
 package com.socialcloud.ms_publicaciones.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PublicacionRequest {
+
+    @NotNull(message = "El usuarioId es obligatorio")
+    private Long usuarioId;
+
+    @NotBlank(message = "El contenido es obligatorio")
+    private String contenido;
 }
