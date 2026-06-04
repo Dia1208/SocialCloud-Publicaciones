6bea9e51d3f849826e3159a640b32f80d779440c
Dia1208 <dia.gonzalezb@duocuc.cl>
2026-06-04 13:15:42 -0400

    Implementar GlobalExceptionHandler para la gestión de errores
    Added global exception handling for runtime and validation errors.

--- ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Exception/GlobalExceptionHandler.java
+++ ms-publicaciones/src/main/java/com/socialcloud/ms_publicaciones/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,42 @@
 package com.socialcloud.ms_publicaciones.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> runtimeException(
+            RuntimeException ex) {
+
+        Map<String, String> error = new HashMap<>();
+
+        error.put("error", ex.getMessage());
+
+        return error;
+    }
+
+    @ExceptionHandler(
+            MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> validationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errores.put(
+                                error.getField(),
+                                error.getDefaultMessage()));
+
+        return errores;
+    }
 }
